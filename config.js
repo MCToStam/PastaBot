dc3063afb3454e462ac2968158675c3bf613b6ea
ToStam <123861440+MCToStam@users.noreply.github.com>
2023-01-29 09:21:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ class config {
     constructor() {
         this.token2 = "TOKEN_BOT"
         this.token = "TOKEN_BOT"
-        this.devid = ["719072181631320145"]
+        this.devid = ["USER_ID"]
         this.bote = 'ToStam'
         this.version = '2.1'
         this.youtube= 'AIzaSyDbMiE_PhC-ydfYNQSPPzEiAr34C0tJlvM'
-        this.logo = 'https://images-ext-2.discordapp.net/external/KG84rt_ZBIRvBwH3r8HC1hQ8eEZPGNT6DQkI3GFpeA8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/902827068901949460/d0351821f73f580b0d218282cfc258b5.webp?width=676&height=676'
+        this.logo = 'LOGO_BOT'
     }
 }
 
